92ef6601b63bb9d1d4f023241dbae6b8c4a99ed4
Veerabhadra <javascript.spry@gmail.com>
2018-10-29 03:28:28 -0700

    first heroku deployement

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname+'/dist'));
+
+app.listen(process.env.PORT || 8080);
+
+// Path location stratogy
+app.get('/*',(req,res) =>{
+	res.sentFile(path.join(__dirname + '/dist/index.html'));
+});
+
+console.log("Console listen!");
